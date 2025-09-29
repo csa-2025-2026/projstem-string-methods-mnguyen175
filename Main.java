70cf8df5eeb97fda0ba7bf2e98ff3d1416a0f735
mnguyen175 <maryannguyen2017@gmail.com>
2025-09-29 18:21:42 +0000

    done

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write your code here
+    lastFirstN("hello", "goodbye", 3);
+    stringManip("yes", "wHat");
+    removeStr("badaboom", "ada"); 
   }
 
   /** Precondition: s1 and s2 are not null
@@ -10,14 +12,23 @@ public class Main
   */
   public static void lastFirstN(String s1, String s2, int n)
   {
-  
+    String output; 
+    String firstN = s1.substring(s1.length() - n);  
+    String lastN = s2.substring(0, n); 
+
+    output = firstN + lastN; 
+    System.out.println(output);
   }
 
   /** Precondition: s1 and s2 are not null
   */
   public static void stringManip(String s1, String s2)
   {
-  
+    String upper1 = s1.toUpperCase();
+    String letter1 = s2.substring(0, 1); 
+    String letter2 = s2.substring(1); 
+    String upper2 = letter1.toUpperCase() + letter2.toLowerCase();
+    System.out.println(upper1 + " " + upper2);
   }
 
   /** Precondition: s1 and s2 are not null
@@ -25,7 +36,11 @@ public class Main
   */
   public static String removeStr(String s1, String s2)
   {
-    String output = null;
-    return output;
+    String output = null; 
+    int num = s1.indexOf(s2);
+    output = s1.substring(0, num) + s1.substring(num + s2.length());
+    System.out.println(output);
+
+    return output; 
   }
 }
